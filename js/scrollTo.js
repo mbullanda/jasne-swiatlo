0702c476922d28223718dd9cd2a55be1b8a5eb9a
Uzytkownik <uzytkownik@kursGIT.pl>
2021-04-27 14:34:18 +0200

    first concept

---
+++ js/scrollTo.js
@@ -0,0 +1,41 @@
+$.scrollTo(0);
+
+$('#start').click(function() {
+        $.scrollTo($('#jasne-swiatlo'), 1200);
+    }
+);
+
+$('#o-autorce-nav').click(function() {
+        $.scrollTo($('#o-autorce'), 1200);
+    }
+);
+
+$('#historie-nav').click(function() {
+        $.scrollTo($('#historie'), 1200);
+    }
+);
+
+$('#grafiki-nav').click(function() {
+        $.scrollTo($('#grafiki'), 1200);
+    }
+);
+
+$('#oferta-nav').click(function() {
+        $.scrollTo($('#oferta'), 1200);
+    }
+);
+
+$('#kontakt-nav').click(function() {
+        $.scrollTo($('#kontakt'), 1200);
+    }
+);
+
+$('#home-nav').click(function() {
+        $.scrollTo($('body'), 1200);
+    }
+);
+
+
+
+
+
